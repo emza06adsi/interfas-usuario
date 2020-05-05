4cc643c2ecdb3757ff69f60cb31d809a1ba09f5c
esteban <emeza06@misena.edu.co>
2020-05-05 18:32:49 -0500

    app para usuarios

---
+++ src/component/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/header.css'
+class Header extends React.Component{
+
+    render(){
+        return(
+            <React.Fragment>
+                <div className="header"></div>
+            </React.Fragment>
+        )
+    }
+
+} 
+export default Header;
---
+++ src/pages/layout.js
@@ -0,0 +1,127 @@
+import React from 'react';
+import './styles/layout.css'
+class Layout extends React.Component{
+
+    render(){
+        return(
+            <React.Fragment>
+                <div className="body">
+                    <header className="header"></header>
+                    <main className="main">
+                    <article className="container-producto">
+                    
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    <div className="div">
+
+                    </div>
+                    
+                    </article>
+                        
+                      
+                    </main>
+                    <footer className="footer"></footer>
+                </div>
+            </React.Fragment>
+        )
+    }
+
+}
+export default Layout;
